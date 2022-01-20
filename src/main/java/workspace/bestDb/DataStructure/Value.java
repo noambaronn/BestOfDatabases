6320c1270ea24a685343b42b806f6786f33ed7d7
noam <noam88888@gmail.com>
2022-01-20 20:50:18 +0200

    Data structures

--- src/main/java/workspace/bestDb/DataStructure/Value.java
+++ src/main/java/workspace/bestDb/DataStructure/Value.java
@@ -1,2 +1,17 @@
-package workspace.bestDb.DataStructure;public enum Value {
+package workspace.bestDb.DataStructure;
+
+import java.util.Date;
+
+public enum Value {
+    Integer(111), String(222), Date(333);
+
+    private int numVal;
+
+    Value(int numVal) {
+        this.numVal = numVal;
+    }
+
+    public int getNumVal() {
+        return numVal;
+    }
 }
